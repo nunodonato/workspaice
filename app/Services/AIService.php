86f17db72aa60a3d8030e839a1e90f0ba26442a2
Nuno Donato <nunodonato@fastmail.com>
2024-07-10 23:50:07 +0100

    no dd

--- app/Services/AIService.php
+++ app/Services/AIService.php
@@ -161,7 +161,6 @@ class AIService
         $response = $this->ai->messages(Client::MODEL_3_5_SONNET, $messages, $this->buildSystemMessage(), $tools, [], 2000);
 
         if ($response['type'] == 'error') {
-            dd($response['error']['message'], $message);
             throw new \Exception($response['error']['message']);
         }
 
